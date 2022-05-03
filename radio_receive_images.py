6df7e2d92102caabdea570fadf24a0f7d13381d3
Brendo <32585448+BrandonAumick@users.noreply.github.com>
2022-05-03 08:50:02 -0400

    Added basic sender and receiver scripts

---
+++ radio_receive_images.py
@@ -0,0 +1,15 @@
+from microbit import *
+import radio
+
+radio.on()
+radio.config(channel=7)
+
+sleep(1000)
+
+while True:
+    
+    packet = radio.receive()
+
+    if packet:
+        print("Receive:", packet)
+        display.show(getattr(Image, packet))
